8b58c25170ebf5533a7d9ee330968779df0592d0
prokrustes <jakubsemric@gmail.com>
2016-10-13 14:53:10 +0200

    string structure added

---
+++ string.h
@@ -0,0 +1,20 @@
+/*
+ * string.h
+ *
+ * File contains structure String and functions needed for strings.
+ *
+ */
+
+#ifndef STRING_H
+#define STRING_H
+
+
+typedef struct {
+    char *buf;
+    unsigned size;  // size of buffer
+} String;
+
+// TODO
+// functions
+
+#endif
