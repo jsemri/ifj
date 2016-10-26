5a422872ebae583a9f85634bb3169f396ecc3219
prokrustes <jakubsemric@gmail.com>
2016-10-26 10:12:57 +0200

    source file and token added

---
+++ globals.h
@@ -0,0 +1,18 @@
+/*
+ * globals.h
+ *
+ */
+
+#ifndef GLOB_H
+#define GLOB_H
+
+#include "token.h"
+#include <stdio.h>
+
+// FIXME
+// put here all global variables which are in use between most of modules
+
+FILE *source;
+T_token *token;
+
+#endif
