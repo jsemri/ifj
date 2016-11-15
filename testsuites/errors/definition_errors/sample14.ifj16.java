3891d472d89e7274d41f600d6dc122d1e24d0b55
Mikyboy <Martin.Mikan@email.cz>
2016-11-15 18:24:32 +0100

    Delete sample14.ifj16.java

--- testsuites/errors/definition_errors/sample14.ifj16.java
+++
@@ -1,9 +0,0 @@
-class Main {
-	static String a;
-    static void run() {
-        static int foo(){
-        	a = "string";
-        	return a; // returning string in int function
-        }
-    }
-}
