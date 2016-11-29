e7d3a104b515c8a09c266d51c5bd2ae7f52f988e
prokrustes <jakubsemric@gmail.com>
2016-11-29 17:21:13 +0100

    Adding some tests for SIMPLE.

---
+++ testsuites/interpret/simple-test1.ifj16.java
@@ -0,0 +1,37 @@
+class Main {
+    static int a = 35;
+    static void run() {
+        if (a > 89)
+            if (a > 23)
+                ifj16.print("rreqw\n");
+            else
+                ifj16.print("rrw\n");
+        else
+            if (a == 0 - a)
+                ifj16.print("w\n");
+            else
+                ifj16.print("rarw\n");
+
+        int cnt = 15;
+        while (cnt > 0)
+            if (cnt > 3) {
+                ifj16.print(cnt + "\n");
+                cnt = cnt - 1;
+            }
+            else
+                cnt = cnt - 1;
+        while (cnt > 100)
+            while (cnt > 100)
+                while (cnt > 100)
+                    cnt = cnt + 1;
+
+        if (5 == 5)
+            if (4 == 4)
+                if (3 == 3)
+                    if ( 2== 2)
+                        if (2 == 1)
+                            ifj16.print("boo\n");
+                        else
+                            ifj16.print("else\n");
+    }
+}
