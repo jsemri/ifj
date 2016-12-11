33fbb710d4755793eac828e614ab806f34e0b25f
Petr Rusiňák <ateps.r@gmail.com>
2016-12-11 14:02:05 +0100

    Asi poslední test :)

---
+++ tests/errors/3_definition_errors/use_function_as_variabe.java
@@ -0,0 +1,13 @@
+//v inicializacii statickej premennej je identifikator funkcie bez zatvoriek
+//navrat 3
+// Autorka testu: Martina G.
+
+class Main {
+	static int x = a;
+	static void run() {
+		
+	}
+	static int a(){
+		int b = 2;
+	}
+}
